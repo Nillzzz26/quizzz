6b9783fc4248f17b5db11777d674c79948c75e53
Nillzzz26 <nurul.ainiltkj2@gmail.com>
2023-05-18 22:41:07 +0700

    Initial commit

---
+++ app/src/main/java/com/ainil/quiz/room/Contant.kt
@@ -0,0 +1,9 @@
+package com.ainil.quiz.room
+
+class Contant {
+    companion object {
+        const val TYPE_READ = 0
+        const val TYPE_CREATE = 1
+        const val TYPE_UPDATE = 2
+    }
+}
---
+++ app/src/main/java/com/ainil/quiz/room/Note.kt
@@ -0,0 +1,13 @@
+package com.ainil.quiz.room
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Skripsi(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val nisn: String,
+    val nama: String,
+    val alamat: String,
+    val asalsekolah: String,
+)
